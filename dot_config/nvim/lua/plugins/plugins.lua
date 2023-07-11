85040bfd1ad0317bfd580191641cd62f89db0702
Bram van Meurs <7422223+bcvanmeurs@users.noreply.github.com>
2023-07-11 11:14:52 +0100

    add flash plugin

--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -9,5 +9,37 @@ return {
         require("telescope").load_extension("fzf")
       end,
     },
+    -- flash options from defaults
+    opts = function(_, opts)
+      local function flash(prompt_bufnr)
+        require("flash").jump({
+          pattern = "^",
+          label = { after = { 0, 0 } },
+          search = {
+            mode = "search",
+            exclude = {
+              function(win)
+                return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "Telescoperesults"
+              end,
+            },
+          },
+          action = function(match)
+            local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+            picker:set_selection(match.pos[1] - 1)
+          end,
+        })
+      end
+      local actions = require("telescope.actions")
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+        mappings = {
+          n = { s = flash },
+          i = {
+            ["<c-s>"] = flash,
+            ["<C-e>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-n>"] = actions.move_selection_next, -- move to next result
+          },
+        },
+      })
+    end,
   },
 }
