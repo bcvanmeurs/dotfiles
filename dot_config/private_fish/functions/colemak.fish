cd2e31db45b8629bb5e585254b54ada2644bafcb
Bram van Meurs <7422223+bcvanmeurs@users.noreply.github.com>
2024-10-21 09:35:31 +0100

    feat: add colemak toggle

--- dot_config/private_fish/functions/colemak.fish
+++ dot_config/private_fish/functions/colemak.fish
@@ -1,33 +1,54 @@
 function colemak
     set vars_file ~/.config/fish/conf.d/vars.fish
 
-    # Check if the user provided an argument (either "true" or "false")
-    if not count $argv >/dev/null
-        echo "Usage: colemak true|false"
-        return 1
+    # Function to display help
+    function display_help
+        echo "Usage: colemak [option]"
+        echo "Options:"
+        echo "  -h, --help    Show this help message"
+        echo "  t, true       Set COLEMAK to true"
+        echo "  f, false      Set COLEMAK to false"
+        echo "  (no option)   Toggle the current value of COLEMAK"
     end
 
-    # Check if the argument is valid (either "true" or "false")
-    if test "$argv[1]" != true -a "$argv[1]" != false
-        echo "Invalid argument. Use 'true' or 'false'."
-        return 1
+    # Function to toggle the current value
+    function toggle_value
+        if test "$COLEMAK" = true
+            set -gx COLEMAK false
+        else
+            set -gx COLEMAK true
+        end
     end
-
-    # Create the line to search for
-    set search_line "set -gx COLEMAK "
-
-    # Construct the new line based on the user's argument
-    set replace_line "set -gx COLEMAK $argv[1]"
-    set -gx COLEMAK $argv[1]
 
     # Check if the vars.fish file exists
-    if test -f "$vars_file"
-        # Replace the COLEMAK line with the new value using quotes around the path
-        sed -i "" "s/^$search_line.*/$replace_line/" "$vars_file"
-        echo "Updated COLEMAK to $argv[1]"
-        return 0
-    else
+    if not test -f "$vars_file"
         echo "Error: $vars_file not found."
         return 1
     end
+
+    # Check if the user provided an argument
+    if not count $argv >/dev/null
+        # No argument provided, toggle the current value
+        toggle_value
+    else
+        switch $argv[1]
+            case -h --help
+                display_help
+                return 0
+            case t true
+                set -gx COLEMAK true
+            case f false
+                set -gx COLEMAK false
+            case '*'
+                echo "Invalid argument. Use 't', 'f', 'true', or 'false'."
+                return 1
+        end
+    end
+
+    # Construct the new line and update the file
+    set replace_line "set -gx COLEMAK $COLEMAK"
+    set -gx COLEMAK $COLEMAK
+    sed -i "" "s/^set -gx COLEMAK .*/$replace_line/" "$vars_file"
+    echo "Updated COLEMAK to $COLEMAK"
+    return 0
 end
