662c2d5efb0319302866c4ac9356871fc599d5f7
Bram van Meurs <7422223+bcvanmeurs@users.noreply.github.com>
2024-11-13 13:32:18 +0000

    feat(nix): add home-manager

--- dot_config/nix-darwin/flake.nix
+++ dot_config/nix-darwin/flake.nix
@@ -5,6 +5,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
@@ -13,6 +15,7 @@
       self,
       nix-darwin,
       nixpkgs,
+      home-manager,
       nix-homebrew,
     }:
     let
