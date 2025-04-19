873811466e9a9bba442ce9c18f3199efaf935f12
Bram van Meurs <7422223+bcvanmeurs@users.noreply.github.com>
2025-04-19 11:43:46 +0200

    feat: only run dmypy when it is installed

--- dot_config/nvim-lazyvim/lua/plugins/lint.lua
+++ dot_config/nvim-lazyvim/lua/plugins/lint.lua
@@ -2,21 +2,29 @@ return {
   {
     "mfussenegger/nvim-lint",
     optional = true,
-    opts = {
-      linters_by_ft = {
-        python = { "dmypy" },
-      },
-      linters = {
-        ["markdownlint-cli2"] = {
-          -- https://github.com/LazyVim/LazyVim/discussions/4094#discussioncomment-10178217
+    opts = function()
+      local linters_by_ft = {}
 
-          args = {
-            "--config",
-            vim.fn.stdpath("config") .. "/lua/plugins/.markdownlint-cli2.yaml",
-            "--",
+      -- Check if mypy is installed before using dmypy
+      if vim.fn.executable("mypy") == 1 then
+        linters_by_ft.python = { "dmypy" }
+      else
+        linters_by_ft.python = {} -- No linter or add an alternative
+      end
+
+      return {
+        linters_by_ft = linters_by_ft,
+        linters = {
+          ["markdownlint-cli2"] = {
+            -- https://github.com/LazyVim/LazyVim/discussions/4094#discussioncomment-10178217
+            args = {
+              "--config",
+              vim.fn.stdpath("config") .. "/lua/plugins/.markdownlint-cli2.yaml",
+              "--",
+            },
           },
         },
-      },
-    },
+      }
+    end,
   },
 }
